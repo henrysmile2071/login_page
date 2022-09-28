8e5788980035d513183bd9cdd66638840c8b2139
henrysmile2071 <henrychen.chc@gmail.com>
2022-09-28 11:50:06 +0800

    feat: index page layout

--- app.js
+++ app.js
@@ -2,6 +2,34 @@ const express = require('express')
 const port = 3000
 const app = express()
 const exphdb = require('express-handlebars')
+app.use(express.static('public'))
+const users = [
+  {
+    firstName: 'Tony',
+    email: 'tony@stark.com',
+    password: 'iamironman'
+  },
+  {
+    firstName: 'Steve',
+    email: 'captain@hotmail.com',
+    password: 'icandothisallday'
+  },
+  {
+    firstName: 'Peter',
+    email: 'peter@parker.com',
+    password: 'enajyram'
+  },
+  {
+    firstName: 'Natasha',
+    email: 'natasha@gamil.com',
+    password: '*parol#@$!'
+  },
+  {
+    firstName: 'Nick',
+    email: 'nick@shield.com',
+    password: 'password'
+  }
+]
 app.engine('hbs', exphdb.engine({ extname: '.hbs' }))
 app.set('view engine', 'hbs')
 app.use(express.urlencoded({ extended: true }))
@@ -13,3 +41,7 @@ app.get('/', (req, res) => {
 app.listen(port, () => {
   console.log(`App is running on http://localhost:${port}`)
 })
+
+function verifyAccount(email, password) {
+
+}
